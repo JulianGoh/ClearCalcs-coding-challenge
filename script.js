d49161550d1e8d192650198f659e58d1fbee8385
Julian Goh <juliangoh5@gmail.com>
2022-03-29 09:40:04 +0800

    29/03/2022 - Remove hard coding of modal messages

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
 "use strict";
 
-
 const modalHeading = document.querySelector(".modal-heading");
 const clickMe = document.querySelector(".click-me");
 const clickYes = document.querySelector(".click-yes");
 const clickCancel = document.querySelector(".click-cancel");
-const modal = document.querySelector('.modal-window');
-const overlay = document.querySelector('.overlay');
-const confirmMsg = document.querySelector('.confirm-msgs');
+const modal = document.querySelector(".modal-window");
+const overlay = document.querySelector(".overlay");
+const confirmMsg = document.querySelector(".confirm-msgs");
 
-clickMe.addEventListener('click', function(){
-  overlay.classList.remove('hidden');
-  modal.style.display = 'flex';
-  modalHeading.innerHTML = 'Are you sure you want to continue?';
+
+const openModal = function (text) {
+  overlay.classList.remove("hidden");
+  modal.style.display = "flex";
+  modalHeading.innerHTML = text;
+};
+
+const closeModal = function(text){
+  overlay.classList.add("hidden");
+  modal.style.display = "none";
+  confirmMsg.insertAdjacentHTML("beforeend", `<p> ${text} </p>`);
+}
+
+clickMe.addEventListener("click", function () {
+  openModal(clickMe.dataset.heading);
 });
 
-clickYes.addEventListener('click', function(){
-  overlay.classList.add('hidden');
-  modal.style.display = 'none';
-  confirmMsg.insertAdjacentHTML('beforeend', 'You just clicked "Yes"');
-})
+clickYes.addEventListener("click", function () {
+  closeModal(clickYes.dataset.heading);
+});
 
-clickCancel.addEventListener('click', function(){
-  overlay.classList.add('hidden');
-  modal.style.display = 'none';
-  confirmMsg.insertAdjacentHTML('beforeend', 'You just clicked "Cancel"');
-})
-
+clickCancel.addEventListener("click", function () {
+  closeModal(clickCancel.dataset.heading);
+});
 
 
 
